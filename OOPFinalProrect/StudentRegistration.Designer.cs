3c848be65831d4a748d891e314368300a20bb849
Lancieeeeeee <jd7730365@gmail.com>
2025-12-07 23:54:23 +0800

    i add new design

---
+++ OOPFinalProrect/StudentRegistration.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace OOPFinalProrect
+{
+    partial class StudentRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.MidnightBlue;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1067, 78);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(373, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(268, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Student Registration";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(56, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(85, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Firstname:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(56, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Lastname:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(56, 222);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Address:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(56, 275);
+            label5.Name = "label5";
+            label5.Size = new Size(71, 21);
+            label5.TabIndex = 4;
+            label5.Text = "Contact:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(56, 325);
+            label6.Name = "label6";
+            label6.Size = new Size(52, 21);
+            label6.TabIndex = 5;
+            label6.Text = "Email:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(159, 128);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(297, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(159, 178);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(297, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(159, 224);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(297, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(159, 277);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(297, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(159, 327);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(297, 23);
+            textBox5.TabIndex = 10;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(554, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 93);
+            button1.TabIndex = 11;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(554, 257);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 93);
+            button2.TabIndex = 12;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(789, 128);
+            button3.Name = "button3";
+            button3.Size = new Size(152, 93);
+            button3.TabIndex = 13;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(789, 257);
+            button4.Name = "button4";
+            button4.Size = new Size(152, 93);
+            button4.TabIndex = 14;
+            button4.Text = "Cancel";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // StudentRegistration
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1067, 547);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Name = "StudentRegistration";
+            Text = "StudentRegistration";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
