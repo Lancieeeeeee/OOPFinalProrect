e5dd6c54fbdcc4351ba38997e168697cbc1bc3fa
LENOVO <LENOVO@DESKTOP-F0MEFBD>
2025-12-15 17:27:33 +0800

    error

--- OOPFinalProrect/StudentRegistration.Designer.cs
+++ OOPFinalProrect/StudentRegistration.Designer.cs
@@ -55,8 +55,9 @@
             panel1.Controls.Add(label1);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(4, 5, 4, 5);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1067, 78);
+            panel1.Size = new Size(1524, 130);
             panel1.TabIndex = 0;
             // 
             // label1
@@ -64,9 +65,10 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ButtonFace;
-            label1.Location = new Point(373, 23);
+            label1.Location = new Point(533, 38);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(268, 37);
+            label1.Size = new Size(407, 55);
             label1.TabIndex = 0;
             label1.Text = "Student Registration";
             // 
@@ -74,9 +76,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(56, 126);
+            label2.Location = new Point(80, 210);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(85, 21);
+            label2.Size = new Size(127, 32);
             label2.TabIndex = 1;
             label2.Text = "Firstname:";
             // 
@@ -84,9 +87,10 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(56, 176);
+            label3.Location = new Point(80, 293);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(83, 21);
+            label3.Size = new Size(125, 32);
             label3.TabIndex = 2;
             label3.Text = "Lastname:";
             // 
@@ -94,9 +98,10 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(56, 222);
+            label4.Location = new Point(80, 370);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(74, 21);
+            label4.Size = new Size(106, 32);
             label4.TabIndex = 3;
             label4.Text = "Address:";
             // 
@@ -104,9 +109,10 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(56, 275);
+            label5.Location = new Point(80, 458);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(71, 21);
+            label5.Size = new Size(105, 32);
             label5.TabIndex = 4;
             label5.Text = "Contact:";
             // 
@@ -114,79 +120,90 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(56, 325);
+            label6.Location = new Point(80, 542);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(52, 21);
+            label6.Size = new Size(78, 32);
             label6.TabIndex = 5;
             label6.Text = "Email:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(159, 128);
+            textBox1.Location = new Point(227, 213);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(297, 23);
+            textBox1.Size = new Size(423, 31);
             textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(159, 178);
+            textBox2.Location = new Point(227, 297);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(297, 23);
+            textBox2.Size = new Size(423, 31);
             textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(159, 224);
+            textBox3.Location = new Point(227, 373);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(297, 23);
+            textBox3.Size = new Size(423, 31);
             textBox3.TabIndex = 8;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(159, 277);
+            textBox4.Location = new Point(227, 462);
+            textBox4.Margin = new Padding(4, 5, 4, 5);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(297, 23);
+            textBox4.Size = new Size(423, 31);
             textBox4.TabIndex = 9;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(159, 327);
+            textBox5.Location = new Point(227, 545);
+            textBox5.Margin = new Padding(4, 5, 4, 5);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(297, 23);
+            textBox5.Size = new Size(423, 31);
             textBox5.TabIndex = 10;
             // 
             // button1
             // 
-            button1.Location = new Point(554, 126);
+            button1.Location = new Point(791, 210);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(152, 93);
+            button1.Size = new Size(217, 155);
             button1.TabIndex = 11;
             button1.Text = "Submit";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(554, 257);
+            button2.Location = new Point(791, 428);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(152, 93);
+            button2.Size = new Size(217, 155);
             button2.TabIndex = 12;
             button2.Text = "Clear";
             button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(789, 128);
+            button3.Location = new Point(1127, 213);
+            button3.Margin = new Padding(4, 5, 4, 5);
             button3.Name = "button3";
-            button3.Size = new Size(152, 93);
+            button3.Size = new Size(217, 155);
             button3.TabIndex = 13;
             button3.Text = "Update";
             button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new Point(789, 257);
+            button4.Location = new Point(1127, 428);
+            button4.Margin = new Padding(4, 5, 4, 5);
             button4.Name = "button4";
-            button4.Size = new Size(152, 93);
+            button4.Size = new Size(217, 155);
             button4.TabIndex = 14;
             button4.Text = "Cancel";
             button4.UseVisualStyleBackColor = true;
@@ -195,24 +212,26 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(56, 372);
+            label7.Location = new Point(80, 620);
+            label7.Margin = new Padding(4, 0, 4, 0);
             label7.Name = "label7";
-            label7.Size = new Size(92, 21);
+            label7.Size = new Size(136, 32);
             label7.TabIndex = 15;
             label7.Text = "Student ID:";
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(159, 374);
+            textBox6.Location = new Point(227, 623);
+            textBox6.Margin = new Padding(4, 5, 4, 5);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(297, 23);
+            textBox6.Size = new Size(423, 31);
             textBox6.TabIndex = 16;
             // 
             // StudentRegistration
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1067, 547);
+            ClientSize = new Size(1524, 912);
             Controls.Add(textBox6);
             Controls.Add(label7);
             Controls.Add(button4);
@@ -230,6 +249,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(panel1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "StudentRegistration";
             Text = "StudentRegistration";
             panel1.ResumeLayout(false);
--- OOPFinalProrect/StudentRegistration.cs
+++ OOPFinalProrect/StudentRegistration.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace OOPFinalProrect
 {
+
     public partial class StudentRegistration : Form
     {
+        // Fix: Move the MySqlConnection declaration inside the class and use correct syntax.
+        public MySqlConnection con = new MySqlConnection("server=localhost; user=root; password=root; database=studentreg_db");
+
         public StudentRegistration()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                Console.WriteLine("Connection Successful");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
